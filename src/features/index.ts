9696d880875912588b62bf1c8f84ae8fc4e23e31
Yan <i@yinyan.fr>
2023-11-13 16:25:45 +0100

    chore: housekeeping (#19)

--- src/features/index.ts
+++ src/features/index.ts
@@ -2,4 +2,5 @@ export { default as Radio } from './radio';
 export { getDailyBonus } from './daily-bonus';
 export { goToBed, hasUserSleptEnough } from './bedtime';
 export { drawComplex, productionSummary } from './cgss-complex';
+export { pickTenCards } from './cgss-simple';
 export { helpText } from './help';
--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,1 +1,4 @@
 export * from './book';
+export * from './sing';
+export * from './cgss';
+export * from './general';
